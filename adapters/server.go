5d21226b1149c74542c9463ec6f39ccf8d44fe07
Michael alive Lipinski <alkyalive@gmail.com>
2025-05-08 14:29:02 +0400

    First iteration

---
+++ adapters/server.go
@@ -0,0 +1,17 @@
+package adapters
+
+import (
+	"vehicles_api/api"
+	"vehicles_api/api/graph/resolvers"
+	"vehicles_api/domain"
+)
+
+type ServerState struct {
+	Resolver *resolvers.Resolver
+}
+
+func BuildServerState(app *domain.App) *api.ServerState {
+	return &api.ServerState{
+		Resolver: resolvers.NewResolver(app.VehicleRepo),
+	}
+} 
